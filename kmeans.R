1da83305c55a308ad744cfefeb2d2b56c8a94c13
Thomas Roiseux <thomas.roiseux@outlook.com>
2022-11-22 17:54:53 +0100

    Init algo kmeans ok

--- kmeans.R
+++ kmeans.R
@@ -1,6 +1,15 @@
 #Step 1
-kmeans-init <- function(d,k){
+kmeans_init <- function(d,k){
     #d is the dimension
     #k is the number of clusters
-    
+    output = list(floor(runif(n = d, min = 0, max = 100)))
+    for (i in 2:k)
+    {
+        x <- floor(runif(n = d, min = 0, max = 100));
+        x <- list(x)
+        output <- append(output, x)
+    }
+    return(output)
 }
+
+#Step 2
